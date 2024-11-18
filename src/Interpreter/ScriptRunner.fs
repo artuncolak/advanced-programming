3b57e86b1977c1b7e7a34287e470754c2be697b3
Artun Colak <artuncolak97@gmail.com>
2024-11-18 07:05:38 +0000

    add line specific errors

--- src/Interpreter/ScriptRunner.fs
+++ src/Interpreter/ScriptRunner.fs
@@ -6,10 +6,18 @@ let run (filePath: string, debug: bool) =
     try
         let lines = IO.File.ReadAllLines(filePath)
 
-        for line in lines do
+        for lineNumber, line in Array.indexed lines do
             if not (String.IsNullOrWhiteSpace(line)) then
-                let _ = Evaluate.run (line, debug)
-                ()
+                try
+                    let _ = Evaluate.run (line, debug)
+                    ()
+                with
+                | ex when ex.Message.StartsWith("Parser error") ->
+                    printfn "Error on line %d: %s" (lineNumber + 1) ex.Message
+                    ()
+                | ex ->
+                    printfn "Error on line %d: %s" (lineNumber + 1) ex.Message
+                    ()
     with
     | :? IO.FileNotFoundException -> printfn "Error: Source file not found at path: %s" filePath
-    | ex -> printfn "Error reading/evaluating file: %s" ex.Message
+    | ex -> printfn "Error reading file: %s" ex.Message
