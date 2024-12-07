b98229d0706a53426b2a58cf67a7362f1a9be9fd
Vanya Robertson <vanya.robertson@icloud.com>
2024-12-07 13:29:01 +0000

    reimplemented clock arithmetic behaviour in modulo

--- src/Parser/Operations.fs
+++ src/Parser/Operations.fs
@@ -50,10 +50,26 @@ let modulo (x: RealNum) (y: RealNum) : RealNum =
     | (String _, _) -> raise (Exception("Argument error: Invalid string operation"))
     | _, Int 0 -> raise (DivideByZeroException "Modulo by zero")
     | _, Float f when f = 0.0 -> raise (DivideByZeroException "Modulo by zero")
-    | Float f1, Float f2 -> Float(f1 % f2)
-    | Float f, Int i -> Float(f % float i)
-    | Int i, Float f -> Float(float i % f)
-    | Int i1, Int i2 -> Int(i1 % i2)
+    | Float f1, Float f2 ->
+      if Float(f1 % f2) < Float(0) then
+        Float(f1 % f2 + f2)
+      else
+        Float(f1 % f2)
+    | Float f, Int i ->
+      if Float(f % float i) < Float(0) then
+        Float(f % float i + float i)
+      else
+        Float(f % float i)
+    | Int i, Float f ->
+      if Float(float i % f) < Float(0) then
+        Float(f % float i + float i)
+      else
+        Float(float i % f)
+    | Int i1, Int i2 ->
+      if Int(i1 % i2) < Int(0) then
+        Int(i1 % i2 + i2)
+      else
+        Int(i1 % i2)
 
 let pow (x: RealNum) (y: RealNum) : RealNum =
     match x, y with
