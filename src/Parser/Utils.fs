681c0c0960b03509f49c6bc54fcd215edf32a802
Vanya Robertson <vanya.robertson@icloud.com>
2025-01-06 12:46:31 +0000

    rectified 3 decimal places

--- src/Parser/Utils.fs
+++ src/Parser/Utils.fs
@@ -3,7 +3,7 @@ module Utils
 open SharedTypes
 
 let formatFloat (f: float) : string =
-    sprintf "%.15f" f
+    sprintf "%.3f" f
 
 let formatResult (result: RealNum) : string =
     match result with
